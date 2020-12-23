aa478eee14fa4ad059fc9180bd1bdf4326dea36e
Garret Bassett <garret.bassett@gmail.com>
2020-12-23 15:39:10 -0700

    adjusted functions for readability / performance per Barry's course

--- scripts/site.js
+++ scripts/site.js
@@ -53,7 +53,7 @@ function initializeLoans() {
 
 function create_UUID(){
     var dt = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = (dt + Math.random()*16)%16 | 0;
         dt = Math.floor(dt/16);
         return (c=='x' ? r :(r&0x3|0x8)).toString(16);
@@ -73,9 +73,9 @@ function bindLoansToDropDown() {
 
 
     for (var i = 0; i < LoanApplicationList.length; i++) {
-        var la = LoanApplicationList[i];
+        let la = LoanApplicationList[i];
 
-        var el = document.createElement("option");
+        let el = document.createElement("option");
         el.textContent = "Application of " + la.ApplicantName;
         el.value = la.Id.toString();
         dropDown.appendChild(el);
@@ -90,10 +90,23 @@ function loadApplication() {
 
     if (la != undefined) {
 
-        var isEmployed = la.Factors[0];
-        var hasKids = la.Factors[1];
-        var hasLoans = la.Factors[2];
-        var hasCreditcards = la.Factors[3];
+        var[
+            isEmployed, 
+            hasKids, 
+            hasLoans, 
+            hasCreditcards
+        ] = la.Factors;
+
+        // var isEmployed = la.Factors[0];
+        // var hasKids = la.Factors[1];
+        // var hasLoans = la.Factors[2];
+        // var hasCreditcards = la.Factors[3];
+
+        var 
+        {
+            Id: a,
+            ApplicantName: b,
+        } = la;
 
 
         document.getElementById("inputName").value = la.ApplicantName;
@@ -111,7 +124,7 @@ function loadApplication() {
 
         var riskLabel = document.getElementById("riskSummary");
         riskLabel.style.display = "block";
-        riskLabel.innerText = generateRickProfile(la); 
+        riskLabel.innerHTML = generateRiskProfile(la); 
 
     }
 }
@@ -190,7 +203,9 @@ function getLoanApplicationDataFromInputs() {
     la.Factors[2] = hasLoans;
     la.Factors[3] = hasCreditcards;    
 
-    if (month != "" && day != "" && year != "") {
+    if (Number.IsInteger(month) 
+    && Number.IsInteger(day) 
+    && Number.IsInteger(year)) {
         la.ApplicantDateOfBirth = new Date(year, month, day);
     }
 
@@ -260,23 +275,29 @@ function validateApplication() {
     return valid;
 }
 
-function generateRickProfile(la) {
+function generateRiskProfile(la) {
     var risk = 3;
 
-    var nameAndTitle = la.ApplicantName;
+    var nameAndTitle = la.ApplicantName.trim().toLowerCase();
 
-    var indexOfMD = nameAndTitle.search("MD");
-    var indexOfMD2 = nameAndTitle.search("M.D");
-    var indexOfMD3 = nameAndTitle.search("M.D.");
-    var indexOfPhD = nameAndTitle.search("PhD");
-    var indexOfPhD2 = nameAndTitle.search("Ph.D");
-    var indexOfPhD3 = nameAndTitle.search("PHD");
-    var indexOfDr = nameAndTitle.search("Dr.");
-    var indexOfDr2 = nameAndTitle.search("DR.");
+    // var indexOfMD = nameAndTitle.search("MD");
+    // var indexOfMD2 = nameAndTitle.search("M.D");
+    // var indexOfMD3 = nameAndTitle.search("M.D.");
+    // var indexOfPhD = nameAndTitle.search("PhD");
+    // var indexOfPhD2 = nameAndTitle.search("Ph.D");
+    // var indexOfPhD3 = nameAndTitle.search("PHD");
+    // var indexOfDr = nameAndTitle.search("Dr.");
+    // var indexOfDr2 = nameAndTitle.search("DR.");
 
-    if (indexOfMD > -1 || indexOfMD2 > -1 || indexOfMD3 > -1 
-        || indexOfPhD > -1 || indexOfPhD2 > -1 
-        || indexOfPhD3 > -1 || indexOfDr > -1 || indexOfDr2 > -1) {
+    let dr = nameAndTitle.startsWith("dr");
+    let phd = nameAndTitle.startsWith("phd");
+    let phd2 = nameAndTitle.startsWith("ph.d");
+
+    let md = nameAndTitle.endsWith("md");
+    let md2 = nameAndTitle.endsWith("m.d");
+    let md3 = nameAndTitle.endsWith("m.d.");
+
+    if (dr || phd || phd2 || md || md2 || md3) {
 
         risk = risk - 1;
     }
@@ -308,7 +329,7 @@ function generateRickProfile(la) {
         risk = risk + 1;
     }
 
-    var purpose = la.LoanPurpose;
+    var purpose = la.LoanPurpose.trim().toLowerCase();
 
     var indexOfHouse = purpose.search("House");
     var indexOfHouse2 = purpose.search("house");
@@ -319,18 +340,22 @@ function generateRickProfile(la) {
     var indexOfBusiness = purpose.search("Business");
     var indexOfBusiness2 = purpose.search("business");
 
-    if (indexOfHouse > -1 || indexOfHouse2 > -1) {
+    let house = purpose.includes("house");
+    let holiday = purpose.includes("holiday");
+    let vacation = purpose.includes("vacation");
+    let business = purpose.includes("business");
+
+    if (house) {
         //the loan will be used for a house or building project
         risk = risk + 2;
     }
 
-    if (indexOfHoliday > -1 || indexOfHoliday2 > -1 
-        || indexOfHoliday3 > -1 || indexOfHoliday4 > -1) {
+    if (holiday || vacation) {
         //the loan will be used for a holiday
         risk = risk + 3;
     }
 
-    if (indexOfBusiness > -1 || indexOfBusiness2 > -1) {
+    if (business) {
         //the loan will be used for a business
         risk = risk + 1;
     }
@@ -339,9 +364,9 @@ function generateRickProfile(la) {
     var reviewText = "";
 
     if (age < 18) {
-        reviewText = "your application will not be reviewed, because you have to be 18 years or older.";
+        reviewText = "your application will not be reviewed, because you have to be 18 years or older";
     } else {
-        reviewText = "your application will be reviewed.";
+        reviewText = "your application will be reviewed";
     }
 
     var riskProfile = "";
@@ -356,9 +381,37 @@ function generateRickProfile(la) {
         riskProfile = "high";
     }
 
-    var summaryText = "Dear " + la.ApplicantName + ", " + reviewText + " Your risk profile is " + riskProfile;
+    var summaryText = highlightText `Dear ${la.ApplicantName},
+    ${reviewText}.
+    Your risk profile is ${riskProfile}
+    Your unique application code is \t${createApplicationID()}`;
 
     return summaryText;
+}
+
+function highlightText(strings, ...values) {
+    let str = "";
+    for(var i = 0; i < strings.raw.length; i++) {
+        if(i > 0) {
+            str += `<b>${values[i - 1]}</b>`
+        }
+        str += strings[i];
+    }
+    return str;
+}
+
+function createApplicationID() {
+
+    var result = '';
+    var characters = 'ABCDEUVYZabcdrswxyz01789/\\#@$%()*^!';
+
+    var charactersLength = characters.length;
+    for (var i = 0; i < 8; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    return result;
+
 }
 
 
